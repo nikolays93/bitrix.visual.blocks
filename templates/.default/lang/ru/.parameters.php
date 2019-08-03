c7a95235d50efdaa407ed2128fc3bd5763ad7533
nikolays93 <nikolays93@ya.ru>
2019-08-03 16:47:28 +0400

    Почистил компонент

--- templates/.default/lang/ru/.parameters.php
+++ templates/.default/lang/ru/.parameters.php
@@ -1,3 +1,1 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-// $MESS["BXCERT_COUNT_ELEMENTS"] = "Количество выводимых элементов";
-?>
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
